b1812d21cac58746722a4fbf3564cbb2462c3bc6
RoberSmith <>
2021-05-14 21:30:43 +0000

    button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById('button').addEventListener('click', compare)
+let button = 0
 let date = 0
 let age = 0
 
@@ -13,7 +14,7 @@ function compare () {
   } else if (date !== 'saturday' || date !== 'sunday' || age > 18) {
     button = document.getElementById('answer').innerHTML = 'Time for work!'
   } else if (date === 'saturday' || date === 'sunday' || age <= 18) {
-    button = document.getElementById('answer').innerHTML = 'you must pay regular price'
+    button = document.getElementById('answer').innerHTML = 'you must pay' regular price'
   } else if (date === 'saturday' || date === 'sunday' || age > 18) {
     button = document.getElementById('answer').innerHTML = 'you must pay regular price'
   }
