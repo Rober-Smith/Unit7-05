a98fa7166d19856f07075bd123118f2beefb3a2a
RoberSmith <>
2021-05-14 20:51:30 +0000

    work pls

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-document.getElementById('button').addEventListener('click',compare)
+document.getElementById('button').addEventListener('click', compare)
 let date = 0
 let age = 0
 
-function compare() {
+function compare () {
   age = document.getElementById('input').value
-  age =  parseInt(age)
+  age = parseInt(age)
   date = document.getElementById('input2').value
-  date =  parseInt(date)
+  date = parseInt(date)
 
-  if (date != "saturday" || date != "sunday" || age <= 18) {
-  userInput = document.getElementById('answer').innerHTML = "Time for school!"
-  } else if (date != "saturday" || date != "sunday" || age > 18) {
-  userInput = document.getElementById('answer').innerHTML = "Time for work!"
-  } else if (date == "saturday" || date == "sunday" || age <= 18) {
-  userInput = document.getElementById('answer').innerHTML = "you must pay regular price"
-  } else if (date == "saturday" || date == "sunday" || age > 18) {
-  userInput = document.getElementById('answer').innerHTML = "you must pay regular price"
+  if (date !== 'saturday' || date !== 'sunday' || age <= 18) {
+    userInput = document.getElementById('answer').innerHTML = "Time for school!"
+  } else if (date !== 'saturday' || date !== 'sunday' || age > 18) {
+    userInput = document.getElementById('answer').innerHTML = "Time for work!"
+  } else if (date === 'saturday' || date === 'sunday' || age <= 18) {
+    userInput = document.getElementById('answer').innerHTML = 'you must pay regular price'
+  } else if (date === 'saturday' || date === 'sunday' || age > 18) {
+    userInput = document.getElementById('answer').innerHTML = 'you must pay regular price'
   }
 }
